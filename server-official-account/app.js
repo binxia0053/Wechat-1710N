d8a3f3a5e1c59e1cda511f1ffda97beec418da66
thu <mingfei.net@gmail.com>
2018-03-22 14:50:38 +0800

    Update

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const wechat = require('wechat');
+const mysql = require('mysql');
 
 let config = {
     appid: 'wxd29115390c39d130',
@@ -7,20 +8,25 @@ let config = {
     encodingAESKey: 'L2BObsKmAmGVWkveoU4HyWGK85BFIQ4zOVpazWpT42C'
 };
 
+let pool = mysql.createPool({user: 'root'});
+
 let app = new express();
 
 app.post('/', wechat(config, (req, res) => {
     let content = req.weixin.Content;
     console.log(content);
-    if (content.includes('JavaScript')) {
-        res.reply('JS...');
-    } else if (content.includes('HTML')) {
-        res.reply('HTML...');
-    } else if (content.includes('CSS')) {
-        res.reply('CSS...');
-    } else {
-        res.reply('收到');
-    }
+    // 根据 content 查询数据库表 chat
+    let sql = `SELECT *
+                FROM db.chat
+                WHERE question LIKE ?`;
+    pool.query(sql, ['%' + content + '%'], (err, results) => {
+        // 返回 answer
+        if (results.length === 1) {
+            res.reply(results[0].answer);
+        } else {
+            res.reply('啊？');
+        }
+    });
 }));
 
 app.listen(3000);
