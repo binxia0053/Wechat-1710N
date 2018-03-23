395f6a598d8d92d318add0d6e8ace8e28511bb88
thu <mingfei.net@gmail.com>
2018-03-23 11:32:58 +0800

    Update

--- server-ip/data/dumpData.js
+++ server-ip/data/dumpData.js
@@ -10,17 +10,25 @@ let eolRegExp =
     process.platform === 'win32' ? /\r\n/ : /\n/;
 
 let sql = `INSERT INTO db.ip(min, max, loc)
-            VALUE(?, ?, ?)`;
+            VALUE ?`;
 let counter = 0;
+let array = []; // 将来是一个二维数组 array[0] = [];
+let index = 0;
 fs.readFileSync(dataPath, 'UTF-8')
     .split(eolRegExp)
-    .forEach(function (line) { // 449767
+    .forEach(function (line) {
         let min = line.split(/\s+/)[0];
         let max = line.split(/\s+/)[1];
         let loc = line.replace(min, '').replace(max, '').trim();
-        pool.query(sql, [min, max, loc], (err, results) => {
-            counter += results.affectedRows;
-            console.log(counter);
-        });
+        array[index++] = [min, max, loc];
     });
+const BATCH = 10000;
+for (let i = 0; i < array.length / BATCH; i++) {
+    let arr = array.slice(BATCH * i, BATCH * (i + 1));
+    pool.query(sql, [arr], (err, results) => {
+        if (err) throw err;
+        counter += results.affectedRows;
+        console.log(counter);
+    });
+}
 // Shift + Alt + F10
