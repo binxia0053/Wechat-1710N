90f22562fbbc99de185b7d4491375e5f79e878f6
thu <mingfei.net@gmail.com>
2018-03-23 10:35:41 +0800

    Update

--- server-ip/data/dumpData.js
+++ server-ip/data/dumpData.js
@@ -3,5 +3,15 @@ const fs = require('fs');
 const path = require('path');
 
 let dataPath = path.join(__dirname, 'ip.data');
+// eol = End Of Line
+let eolRegExp = process.platform === 'win32' ? /\r\n/ : /\n/;
 
-// todo 按行读取文件
+fs.readFileSync(dataPath, 'UTF-8')
+    .split(eolRegExp)
+    .forEach(function (line) { // 449767
+        let min = line.split(/\s+/)[0];
+        let max = line.split(/\s+/)[1];
+        let loc = line.replace(min, '').replace(max, '').trim();
+
+    });
+// Shift + Alt + F10
