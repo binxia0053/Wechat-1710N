d9bbb98ca65f4e2763307044867d85a856a9600f
thu <mingfei.net@gmail.com>
2018-03-23 16:44:19 +0800

    Update

---
+++ wechat-app/pages/map/map.js
@@ -0,0 +1,93 @@
+// pages/map/map.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    longitude: 116.3325,
+    latitude: 40,
+    scale: 10,
+    markers: [
+      {
+        longitude: 116.3325,
+        latitude: 40.5,
+        iconPath: '/resources/img/pin.png',
+        width: 20,
+        height: 30
+      }
+    ],
+    polyline: [
+      {
+        points: [
+          {
+            longitude: 116.3325,
+            latitude: 40.6,
+          },
+          {
+            longitude: 116.3325,
+            latitude: 40.7,
+          }
+        ],
+        color: '#ff0000',
+        width: 1
+      }  
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
