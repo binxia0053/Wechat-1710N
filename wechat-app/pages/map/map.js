473033c80a6591c2cde62469a0b55d763fc72028
thu <mingfei.net@gmail.com>
2018-03-23 17:58:15 +0800

    Update

--- wechat-app/pages/map/map.js
+++ wechat-app/pages/map/map.js
@@ -7,7 +7,7 @@ Page({
   data: {
     longitude: 116.3325,
     latitude: 40,
-    scale: 10,
+    scale: 14,
     markers: [
       {
         longitude: 116.3325,
@@ -39,63 +39,99 @@ Page({
         ],
         color: '#00ff00',
         width: 10
-      }  
+      }
+    ],
+    controls: [
+      {
+        id: 1,
+        position: {
+          left: 30,
+          top: 30
+        },
+        iconPath: '/resources/img/position.png',
+        clickable: true
+      }
     ]
+  },
+
+  controlTap: function (event) {
+      switch(event.controlId) {
+        case 1:
+          console.log('1...');
+          break;
+        default:
+          // ...
+          break;
+      }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let page = this;
+    wx.getLocation({
+      success: function (res) {
+        console.log(res);
+        page.setData({
+          markers: [
+            {
+              longitude: res.longitude,
+              latitude: res.latitude,
+              iconPath: '/resources/img/pin.png'
+            }
+          ]
+        });
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
